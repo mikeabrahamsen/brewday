51a16021f0d753c39e661fcb9692334527aa0262
Mike Abrahamsen <mail@michaelabrahamsen.com>
2015-01-23 22:38:12 -0700

    more user unit tests

--- server/app/tests/test_user.py
+++ server/app/tests/test_user.py
@@ -5,7 +5,6 @@ from unittest import TestCase
 import json
 from config import basedir
 from app import app, db
-from app.models import User
 
 
 class UserRouteTests(TestCase):
@@ -20,6 +19,8 @@ class UserRouteTests(TestCase):
         self.app = app.test_client()
         db.create_all()
 
+        self.user_route = '/api/v1/users'
+
     def tearDown(self):
         db.session.remove()
         db.drop_all()
@@ -28,10 +29,9 @@ class UserRouteTests(TestCase):
         self.assertEqual(headers['Content-Type'], 'application/json')
 
     def test_get_response(self):
-        rv = self.app.get('/api/v1/users')
+        rv = self.app.get(self.user_route)
         self.check_content_type(rv.headers)
         self.assertEqual(rv.status_code, 200)
-
         response = json.loads(rv.data)
 
         # make sure there are no users
@@ -39,13 +39,38 @@ class UserRouteTests(TestCase):
 
     def test_adding_user(self):
         user_data = {'email': 'test@test.com', 'password': 'admin'}
-        rv = self.app.post('/api/v1/users',data=user_data)
+        rv = self.app.post(self.user_route, data=user_data)
         self.check_content_type(rv.headers)
 
-        rv = self.app.get('/api/v1/users')
+        rv = self.app.get(self.user_route)
         self.assertEqual(rv.status_code, 200)
         response = json.loads(rv.data)
         self.assertEqual(len(response), 1)
+
+        # add a second user
+        user_data = {'email': 'test2@test.com', 'password': 'admin'}
+        rv = self.app.post(self.user_route, data=user_data)
+
+        self.check_content_type(rv.headers)
+
+        rv = self.app.get(self.user_route)
+        self.assertEqual(rv.status_code, 200)
+        response = json.loads(rv.data)
+        self.assertEqual(len(response), 2)
+
+        # test adding duplicate user
+        user_data = {'email': 'test2@test.com', 'password': 'admin'}
+        rv = self.app.post(self.user_route, data=user_data)
+
+        # should recieve an error code 422
+        self.assertEqual(rv.status_code, 422)
+        self.check_content_type(rv.headers)
+
+        # another user should not be in the database
+        rv = self.app.get(self.user_route)
+        self.assertEqual(rv.status_code, 200)
+        response = json.loads(rv.data)
+        self.assertEqual(len(response), 2)
 
 if __name__ == '__main__':
         unittest.main()
