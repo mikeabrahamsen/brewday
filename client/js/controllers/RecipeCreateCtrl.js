b89a60ac0805754ad456928fb1b34feaf21b2aad
Mike Abrahamsen <mail@michaelabrahamsen.com>
2015-01-26 21:02:54 -0700

    add grain form

--- client/js/controllers/RecipeCreateCtrl.js
+++ client/js/controllers/RecipeCreateCtrl.js
@@ -1,6 +1,11 @@
 Brewday.controller('RecipeCreateCtrl',  ['$scope', '$location', '$window', 'Recipe',
     function($scope, $location, $window, Recipe){
+        $scope.grains = [{id: 'grain1'},{id: 'grain2'}];
         $scope.data = {};
+        $scope.addNewGrain= function() {
+            var newItemNo = $scope.grains.length+1;
+            $scope.grains.push({'id':'grain'+newItemNo});
+        };
         $scope.submit_recipe = function submit_recipe(name,beertype){
             var recipe = {};
             recipe.name= name;
