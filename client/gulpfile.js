71e366b6e464a5d4d8da43b5131a5260d5c4c8b6
Mike Abrahamsen <mail@michaelabrahamsen.com>
2015-02-03 11:23:56 -0700

    add browser-sync

--- client/gulpfile.js
+++ client/gulpfile.js
@@ -1,5 +1,13 @@
 var gulp = require('gulp');
+var browserSync = require('browser-sync');
 
 gulp.task('default', function() {
     // place code for your default task here
+});
+gulp.task('browser-sync', function() {
+    browserSync({
+        server: {
+            baseDir: "./"
+        }
+    });
 });
