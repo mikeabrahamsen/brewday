6fc83440d56c4cb7d65ce7d36377796d1da7f039
Mike Abrahamsen <mail@michaelabrahamsen.com>
2015-02-03 12:52:53 -0700

    modify gulpfile to watch for changes

--- client/gulpfile.js
+++ client/gulpfile.js
@@ -1,13 +1,19 @@
 var gulp = require('gulp');
 var browserSync = require('browser-sync');
 
-gulp.task('default', function() {
-    // place code for your default task here
+// use default task to launch BrowserSync and watch JS files
+gulp.task('default', ['browser-sync'], function () {
+
+    // add browserSync.reload to the tasks array to make
+    // all browsers reload after tasks are complete.
+    // gulp.watch("js/*.js", ['js', browserSync.reload]);
 });
+
 gulp.task('browser-sync', function() {
     browserSync({
         server: {
             baseDir: "./"
-        }
+        },
+        files: ["css/*.css", "js/**/*.js", "partials/**"]
     });
 });
