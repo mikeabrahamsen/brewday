83db40ed4df77cdbb0ebaed51261d726e94f2ae1
Mike Abrahamsen <mail@michaelabrahamsen.com>
2015-02-09 19:22:32 -0700

    delete the js folder
    - move the service and directives into their new locations

--- client/app/recipes/hops/hops.js
+++ client/app/recipes/hops/hops.js
@@ -11,3 +11,29 @@ Brewday.directive('hopListForm', function(){
         templateUrl: 'app/recipes/hops/hopForm.html'
     }
 })
+Brewday.controller('HopFormCtrl', ['Hop',
+        function(Hop){
+        var original_hops = []
+        if(this.hops.length < 1)
+            this.hops = [{id: 'hop1'}];
+        else
+            original_hops = this.hops.slice(0);
+
+        this.hop_options = Hop.getAll().$object;
+
+        this.addNewHop= function() {
+            var newItemNo = this.hops.length+1;
+            this.hops.push({'id':'hop'+newItemNo});
+        };
+
+        this.removeHop = function(hop) {
+            // if the grain to be deleted is part of the recipe
+            // flag it for deletion
+            if (original_hops.indexOf(hop) > -1)
+            {
+                this.toDelete.push(hop);
+            }
+            this.hops.splice( this.hops.indexOf(hop), 1 );
+        };
+        }
+])
--- client/app/sessions/sessions.js
+++ client/app/sessions/sessions.js
@@ -37,3 +37,25 @@ Brewday.service('AuthService', AuthService = function($q, localStorageService, S
 
     return this;
 })
+Brewday.controller('SessionCreateCtrl',  ['$scope', '$state', 'AuthService',
+    function($scope, $state, AuthService ){
+        $scope.data = {};
+        $scope.login = function login(email, password){
+
+            var user = {'email': '', 'password': ''};
+            user.email = email;
+            user.password = password;
+
+             AuthService.login(user).then(function() {
+                $state.go('recipes.list')
+            });
+        }
+    }
+])
+Brewday.controller('SessionDestroyCtrl',  ['$scope', '$state', 'AuthService',
+    function($scope, $state, AuthService ){
+            AuthService.logout();
+            $scope.isLoggedIn = false;
+            $state.go('home')
+        }
+])
