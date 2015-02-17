3d4aba26afca9f7a4bdec79ab9947c4103099d4f
Mike Abrahamsen <mail@michaelabrahamsen.com>
2015-02-16 22:46:08 -0700

    add tests for grains controller

--- client/app/recipes/grains/grains.spec.js
+++ client/app/recipes/grains/grains.spec.js
@@ -1,20 +1,24 @@
 describe("Grains", function() {
   var element;
+  var $compile;
   var $scope;
+  var controller;
   beforeEach(module("recipes.grains"));
   beforeEach(module("brewday.models.grain"));
+  beforeEach(module("brewday.templates"));
   beforeEach(module("restangular"));
-  beforeEach(inject(function($compile, $rootScope, $httpBackend) {
-    $httpBackend.expect('GET', 'app/recipes/grains/grainForm.html').respond('<div class="col-md-6"> <ng-form name="grainForm" ng-model="grainform.grains"> ');
+  beforeEach(inject(function(_$compile_, $rootScope, $httpBackend) {
     $httpBackend.expect('GET', '/grains').respond('');
     $scope = $rootScope;
+    $compile = _$compile_;
     $scope.data = {
       grains: [],
       toDelete: []
     };
     element = angular.element("<grain-list-form grains='data.grains' to-delete='data.toDelete'></grain-list-form>");
-    $compile(element)($rootScope.$new());
+    $compile(element)($rootScope);
     $httpBackend.flush();
+    controller = element.controller('grainListForm');
   }));
 
   describe("grainListForm directive", function() {
@@ -54,6 +58,58 @@ describe("Grains", function() {
       expect($scope.data.toDelete.length).toBe(3);
       expect(element.scope().data.toDelete.length).toBe(3);
       expect(element.isolateScope().grainform.toDelete.length).toBe(3);
+    });
+  });
+  describe("GrainFormController ", function() {
+    beforeEach(inject(function($httpBackend) {
+      $httpBackend.expect('GET', '/grains').respond('');
+    }));
+    it('will be on the element', function() {
+      expect(element.controller('grainListForm')).toBeDefined();
+      expect(controller).toBeDefined();
+    });
+    it('will have grains', function() {
+      expect(controller.grains).toBeDefined;
+      expect(controller.grains.length).toBe(1);
+    });
+    it('will have a toDelete list', function() {
+      expect(controller.toDelete).toEqual([]);
+    });
+    it('will have grain_options', function() {
+      expect(controller.grain_options).toBeDefined();
+      expect(controller.grain_options.length).toBe(0);
+    });
+    it('will add a new grain', function() {
+      controller.addNewGrain();
+      controller.addNewGrain();
+      expect(controller.grains.length).toBe(3);
+    });
+    it('will delete a grain', function() {
+      expect(controller.grains.length).toBe(1);
+      controller.removeGrain(controller.grains[0]);
+      expect(controller.grains.length).toBe(0);
+    });
+    it('will not put the default grain in the toDelete list', function(){
+      controller.removeGrain(controller.grains[0]);
+      expect(controller.toDelete.length).toBe(0);
+    });
+    it('will put not put a grain that was not in the db in toDelete', function() {
+      var grain = controller.addNewGrain();
+      controller.removeGrain(grain);
+      expect(controller.toDelete.length).toBe(0);
+    });
+    it('will add a grain to toDelete if grain was in the original list', function() {
+        $scope.data.grains = [{name: 'test-grain'},{}];
+        $compile(element)($scope);
+        $scope.$apply();
+
+        controller = element.controller('grainListForm');
+        expect(controller.grains.length).toBe(2);
+        controller.removeGrain(controller.grains[1]);
+        expect(controller.toDelete.length).toBe(1);
+        var grain = controller.addNewGrain();
+        controller.removeGrain(controller.grains[0]);
+        expect(controller.toDelete.length).toBe(2);
     });
   });
 });
