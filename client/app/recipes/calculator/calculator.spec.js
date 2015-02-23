30353bf2b9c3bcf0293ea73e6fbe0ed3d75ee0c6
Mike Abrahamsen <mail@michaelabrahamsen.com>
2015-02-23 16:49:27 -0700

    test default calculator functions

--- client/app/recipes/calculator/calculator.spec.js
+++ client/app/recipes/calculator/calculator.spec.js
@@ -1,8 +1,10 @@
 /* global inject */
-describe('CalculatorService', function() {
+describe('Calculator Service', function() {
   var CalculatorService;
+  var scope;
   beforeEach(module('recipes.calculator'));
-  beforeEach(inject(function(_CalculatorService_) {
+  beforeEach(inject(function(_$rootScope_, _CalculatorService_) {
+    scope = _$rootScope_;
     CalculatorService = _CalculatorService_;
   }));
   describe('Initial Values', function() {
@@ -28,5 +30,46 @@ describe('CalculatorService', function() {
       expect(CalculatorService.mashThickness).toEqual(1.33);
     });
   });
+  describe('calculator', function() {
+    it('should return the correct volumes with default values', function() {
+      CalculatorService.calculateWaterVol(10);
+      expect(CalculatorService.totalVol).toBe(8.67);
+      expect(CalculatorService.mashVol).toBe(3.33);
+      expect(CalculatorService.spargeVol).toBe(5.34);
+    });
+  });
+});
 
+describe('Calculator Directive', function() {
+  var CalculatorService;
+  var $scope;
+  var $compile;
+  var $httpBackend;
+  var element;
+  var controller;
+  beforeEach(module("brewday.templates"));
+  beforeEach(module('recipes.calculator'));
+  beforeEach(inject(function(_$compile_, _$rootScope_, _$httpBackend_) {
+    $scope = _$rootScope_;
+    $httpBackend = _$httpBackend_;
+    $compile = _$compile_;
+    $scope.data = {
+      grains: [{amount: 9},{amount: 1}]
+    };
+    element = angular.element('<water-calculations grains="data.grains"></water-calculations>');
+    $compile(element)($scope);
+    $scope.$apply();
+    controller = element.controller('waterCalculations');
+  }));
+  describe('Calculator Directive Controller', function() {
+    it('should be defined', function() {
+      expect(controller).toBeDefined();
+    });
+    it('should have a CalculatorService', function() {
+      expect(controller.calcService).toBeDefined();
+    });
+    it('should calulate the grain bill', function() {
+      expect(controller.grainBill).toBe(10);
+    });
+  });
 });
