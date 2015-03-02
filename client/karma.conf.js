ee73431ece0221e0b37a8f82c8d0bace374752ba
Mike Abrahamsen <mail@michaelabrahamsen.com>
2015-03-02 14:30:56 -0700

    shouldn't preprocess test files

--- client/karma.conf.js
+++ client/karma.conf.js
@@ -36,8 +36,8 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
-        'app/**/*.js': ['coverage'],
-         "app/**/*.html": ["ng-html2js"]
+        'app/**/!(*spec).js': ['coverage'],
+        "app/**/*.html": ["ng-html2js"]
     },
     ngHtml2JsPreprocessor: {
         moduleName: "brewday.templates"
